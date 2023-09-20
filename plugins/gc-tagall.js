bb6311feed8e1c1f75545e461a31f2debe7d5ed5
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2023-09-20 14:53:09 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,17 +1,21 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}
-let pesan = args.join` `
-let oi = `ღ ${lenguajeGB['smsAddB5']()} ${pesan}`
-let teks = `*𝙇𝙄𝙎𝙏𝘼 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝘼 🍭*\n\n`
-for (let mem of participants) {
-teks += `🏓⊹ @${mem.id.split('@')[0]}\n`}
-teks += `╰━━━━━[ ALCA BOT ]━━━━━⬣`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
-}
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
-handler.group = true
-export default handler
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
+  let teks = `*⺀𝗟𝗜𝗦𝗧𝗔 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗔⺀*\n\n➪ 𝗔𝗟𝗖𝗔𝗕𝗢𝗧\n\n➪ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  for (const mem of participants) {
+    teks += `┣☢︎︎ @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `* 𝐁𝐲 𝗔𝗟𝗖𝗔-𝗕𝗢𝗧\n\n*`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
