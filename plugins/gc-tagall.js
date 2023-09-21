16d6a2babdff5cd05a6d1982168445f16bc4bdbc
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2023-09-20 21:17:51 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,9 +6,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝗟𝗜𝗦𝗧𝗔 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗔⺀*\n\n➪ 𝗔𝗟𝗖𝗔𝗕𝗢𝗧\n\n➪ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*⺀𝙈𝙀𝙉𝘾𝙄𝙊́𝙉 𝙂𝙀𝙉𝙀𝙍𝘼𝙇 ⺀*\n\n➪ 𝗔𝗟𝗖𝗔𝗕𝗢𝗧\n\n➪ *𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼 :*\n`;
   for (const mem of participants) {
-    teks += `📳 @${mem.id.split('@')[0]}\n`;
+    teks += `🦋 @${mem.id.split('@')[0]}\n`;
   }
   teks += `* 𝐁𝐲 𝗔𝗟𝗖𝗔-𝗕𝗢𝗧\n\n*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
