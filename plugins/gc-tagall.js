7c1ea25e370293f553872dac4bec10225fbbfc87
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2023-09-25 01:19:39 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -10,7 +10,6 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `⭐️ @${mem.id.split('@')[0]}\n`;
   }
-   teks += `* 𝙲 𝚁 𝙴 𝙰 𝚃 𝙾 𝚁 \n\n*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
