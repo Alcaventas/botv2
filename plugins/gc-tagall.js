b3369635320a8aa2cec590d891fe4923db9724a4
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2023-10-13 03:50:21 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,7 +6,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* 𝙰𝙻𝙲𝙰𝙱𝙾𝚃🦋`;
-  let teks = `*𝙼𝙴𝙽𝙲𝙸𝙾𝙽 𝙶𝙴𝙽𝙴𝚁𝙰𝙻*\n\n 𝙰𝙻𝙲𝙰-𝙱𝙾𝚃 \n\n💀 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*𝙼𝙴𝙽𝙲𝙸𝙾𝙽 𝙶𝙴𝙽𝙴𝚁𝙰𝙻*\n\n 👻𝙰𝙻𝙲𝙰-𝙱𝙾𝚃 \n\n💀 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
     teks += `🎃 @${mem.id.split('@')[0]}\n`;
   }
